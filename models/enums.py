90168d4e90d993885e8a0dc5dbd6f430a92fbe17
Nicolas <nicolas.richard@epfedu.fr>
2022-09-08 17:29:28 -0400

    path to stanford bioMod

--- models/enums.py
+++ models/enums.py
@@ -48,3 +48,5 @@ class Models(Enum):
 
     WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_ROTOTRANS_SUPPORT_TEMPLATE = Path(__file__).parent.__str__() + "/KINOVA_merge_without_floating_base_with_rototrans_template.bioMod"
     WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_ROTOTRANS_SUPPORT_VARIABLES = Path(__file__).parent.__str__() + "/KINOVA_merge_without_floating_base_with_rototrans_template_with_variables.bioMod"
+
+    Stanford_VA_upper_limb_model_0_40 = Path(__file__).parent.__str__() + "/Stanford_VA_upper_limb_model_0_40.bioMod"
